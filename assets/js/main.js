aad20f9050ad0ed178744d86919d40f6fc211bfe
donneystark <methadookdik@gmail.com>
2024-02-14 11:33:37 +0700

    add blog-detail

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,28 @@
-$( window ).on('scroll', function(){
-    var scrollTop = $(this).scrollTop()
-    var score = '15px'
-    if (scrollTop > 1) {
-        score = '5px 15px'
+$(window).on("scroll", function () {
+  var scrollTop = $(this).scrollTop();
+  var score = "15px";
+  if (scrollTop > 1) {
+    score = "5px 15px";
+  }
+  $("#navbar").css("padding", score);
+});
+
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    responsive:{
+        0:{
+            items:1
+            
+        },
+        600:{
+            items:2
+            
+        },
+        1000:{
+            items:3
+            
+        }
     }
-    $('#navbar').css('padding', score)
+  });
 });
